2e82330318a444684526b68b59ee4f6eefeceac8
Phoenix616 <max@themoep.de>
2024-04-04 15:43:04 +0100

    Fix possible NPE when loading config on Velocity

--- velocity/src/main/java/de/themoep/connectorplugin/velocity/PluginConfig.java
+++ velocity/src/main/java/de/themoep/connectorplugin/velocity/PluginConfig.java
@@ -165,7 +165,7 @@ public class PluginConfig {
     }
 
     public String getString(String path) {
-        return getString(path, null);
+        return getString(path);
     }
 
     public String getString(String path, String def) {
